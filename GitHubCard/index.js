d87e27b18ef04397b679dc844ee7ab68a5e755f1
Sam Kester <samkester@gmail.com>
2020-08-06 13:13:49 -0600

    axios setup; fetch API request

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,12 @@
+import axios from "axios";
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+const myProfile = axios.get("https://api.github.com/users/samkester").then(result => console.log(result));
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
